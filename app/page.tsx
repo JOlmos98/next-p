1f333cf244339ea0c4739f8a011b01a95d202cf5
JOlmos98 <soler98@hotmail.es>
2024-12-14 11:46:28 +0100

    fix dockerfile ya funciona y se crea la imagen. 14/12/24

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 //import Image from "next/image";
 
+import Link from "next/link";
+
 /*const component1=()=>{
   const saludar=()=>{
     //const nombre = document.getElementById('nombre').value;
@@ -14,12 +16,13 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <span className="flex text-3xl">Soy Jesús Olmos Soler, desarrollador con diversos lenguajes y tecnologías y bla bla bla. Ahora, introduce 
           tu nombre en el recuadro de abajo y ¡clicka!
+
         </span>
         <input className="text-black block mx-auto p-3 rounded-3xl" type="text" id="nombre" placeholder="Escribe tu nombre aquí" ></input>
         <button /*</main>onClick={saludar}*/ className="flex bg-blue-300 bg-opacity-30 p-4 mx-auto rounded-3xl text-3xl">Enviar</button>
-        <a className="flex bg-blue-300 bg-opacity-30 p-5 m-2 rounded-3xl text-5xl" href="/about">About me</a>
-        <a className="flex bg-blue-300 bg-opacity-30 p-5 m-2 rounded-3xl text-5xl" href="/pricing">Pricing</a>
-        <a className="flex bg-blue-300 bg-opacity-30 p-5 m-2 rounded-3xl text-5xl" href="/contact">Contact</a>
+        <Link className="flex bg-blue-300 bg-opacity-30 p-5 m-2 rounded-3xl text-5xl" href="/about">About me</Link>
+        <Link className="flex bg-blue-300 bg-opacity-30 p-5 m-2 rounded-3xl text-5xl" href="/pricing">Pricing</Link>
+        <Link className="flex bg-blue-300 bg-opacity-30 p-5 m-2 rounded-3xl text-5xl" href="/contact">Contact</Link>
 
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">

860725c4b2dbc7a9e5da608dbf5a228fc6fd951e
JOlmos98 <soler98@hotmail.es>
2024-12-15 14:02:55 +0100

    WIP: dashboard, Images dado y Iconos implementados.

---
+++ app/dashboard/main/page.tsx
@@ -0,0 +1,9 @@
+// prc = Page React Component
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>Hello Page</h1>
+    </div>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
     //output: 'export',
     output: 'standalone', //En el repo de dockerfile lo recomiendan
+
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+      ],
+      domains: ['images.unsplash.com'],
+    },
+  
   }
   
   export default nextConfig;

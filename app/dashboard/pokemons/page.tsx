588348a2560ab070d4362bdb20344fb819dc214d
JOlmos98 <soler98@hotmail.es>
2024-12-18 08:35:38 +0100

    WIP: conexión a pokeapi hecha. 18/12/24

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -1,8 +1,27 @@
+const getPokemons = async (limit = 20, offset = 0) => {
+  const data = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
 
-export default function PokemonsPage() {
+  return data;
+};
+
+/*
+Aquí hacemos un fetch sencillo de la API
+de pokemons y lo representamos con un JSON 
+parseado a string (stringify).
+
+El async y await nos permiten hacerlo del lado del servidor, creo.
+*/
+
+export default async function PokemonsPage() {
+
+  const pokemons = await getPokemons();
+
   return (
     <div>
       <h1>POKEMONS Page</h1>
+      {JSON.stringify(pokemons)}
     </div>
   );
 }

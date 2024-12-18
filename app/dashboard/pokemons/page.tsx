5a0941c8b3c789d5318a094a39dfa0ffc0c803d2
JOlmos98 <soler98@hotmail.es>
2024-12-18 20:00:53 +0100

    WIP: página de pokemon muestra imágenes mapeadas según los parámetros que extrae del JSON usando las interfaces. 18/12/24

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -1,9 +1,17 @@
-const getPokemons = async (limit = 20, offset = 0) => {
-  const data = await fetch(
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
+
+const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   ).then((res) => res.json());
 
-  return data;
+  const pokemons = data.results.map(pokemon => ({
+    id: Number(pokemon.url.split('/').at(-2)),
+    name: pokemon.name!,
+  }))
+
+  return pokemons;
 };
 
 /*
@@ -16,12 +24,38 @@ El async y await nos permiten hacerlo del lado del servidor, creo.
 
 export default async function PokemonsPage() {
 
-  const pokemons = await getPokemons();
+  const pokemons = await getPokemons(151);
 
   return (
-    <div>
+    <div className="flex flex-col">
       <h1>POKEMONS Page</h1>
-      {JSON.stringify(pokemons)}
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+
+        {
+          pokemons.map(pokemon => (
+            <Image
+              key={pokemon.id}
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+              alt={pokemon.name}
+              width={100}
+              height={100}
+            />
+          ))
+
+        }
+
+
+
+      </div>
     </div>
   );
 }
+
+/*
+        <Image  
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg`}
+          alt="pokemon"
+          width={100}
+          height={100}
+        />
+*/
